9bb3ea0d9a88dba66e971f4cb7f10be61f566307
Hunter Callaway <HunterCallaway@hotmail.com>
2021-09-16 23:05:11 -0500

    Installed Django REST framework and django-cors-headers and created the serializers file.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -43,10 +43,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'luggage.apps.LuggageConfig'
+    'luggage.apps.LuggageConfig',
+    'corsheaders',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -130,3 +133,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Whitelisting React port
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+]
---
+++ backend/luggage/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from .models import User, Trip, LuggageItem
+
+# This REST serializer converts our model classes to JSON for the client
+# and helps make CRUD requests with a JSON payload to the API.
+
+class LuggageItemSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = LuggageItem
+    fields = ['item', 'quantity', 'departure_checked', 'return_checked']
+
+class TripSerializer(serializers.ModelSerializer):
+  # The following serializers are related to the fields from the Django User model. 
+  username = serializers.CharField(source='user.username')
+  email = serializers.CharField(source='user.email')
+  password = serializers.CharField(source='user.password', write_only=True)
+
+  # The following serializer represents a nested relationship.
+  luggage_items = LuggageItemSerializer(many=True, read_only=true)
+
+  class Meta:
+    model = Trip
+    fields = ('username', 'email', 'password', 'destination', 'departure_date', 'return_date', 'created', 'luggage_items')
+
+
+
