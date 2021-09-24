874978c41a66c2a13d0d5c36b5b27b3b24bbe438
Hunter Callaway <HunterCallaway@hotmail.com>
2021-09-24 00:06:45 -0500

    Updated 'backend/urls.py'.

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,8 +14,24 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
+from luggage import views
+from rest_framework import routers
+
+# Declaration of the `router` object
+router = routers.DefaultRouter()
+
+# The `register` method we will use with the `router` object has two mandatory arguments:
+# 1. `prefix`: the URL prefix we will use for this set of routes (preceded by an 'r')
+# 2. `viewset`: the viewset class
+# We will also use the optional `basename` argument to set a base for the URLs that will be created.
+router.register(r'trips', views.TripView, 'trip')
+router.register(r'items', views.LuggageItemView, 'item-list')
+
+# The new 'api' route will take us to the "Django REST framework" website,
+# where we will be able to view our model instances as JSON objects.
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls))
 ]
